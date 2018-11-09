d9fdcec4babcfcf3a109dcb1b7f7f796dfe469c7
jchulim <41027379+jchulim@users.noreply.github.com>
2018-11-09 16:36:25 -0600

    Add files via upload

---
+++ reports/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'nova_reports',
+    'author': 'EMPAQUES NOVA',
+    'website': 'http://www.empaquesnova.com.mx',
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+    'depends': [
+    ],
+    'data': [
+        
+    ],
+}
